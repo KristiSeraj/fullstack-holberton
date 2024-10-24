90041ba2967e8fa585a050c1307e73b7c02f04bf
KristiSeraj <serajkristi@gmail.com>
2024-10-24 16:42:40 +0200

    Book context create context added

--- frontend/src/context/BookContext.js
+++ frontend/src/context/BookContext.js
@@ -1,14 +1,10 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-<<<<<<< HEAD
-import { update_book_service, get_books_service} from "../services/books";
-=======
-import { update_book_service, get_books_service } from "../services/books";
->>>>>>> upstream/main
+import {update_book_service, get_books_service, create_book_service} from "../services/books";
+
 
 const BookContext = createContext({});
 
 const BookProvider = (props) => {
-<<<<<<< HEAD
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
@@ -27,6 +23,19 @@ const BookProvider = (props) => {
         }
     }
 
+    const createBooks = async (data) => {
+        try {
+            const result = await create_book_service(data)
+            console.log(result)
+            if (result.status === 201) {
+                await getBooks();
+            }
+        } catch (error) {
+            console.log("error--in post method--", error);
+            return error
+        }
+    }
+
     const updateBook = async (data) => {
         try {
             const result = await update_book_service(data);
@@ -39,50 +48,12 @@ const BookProvider = (props) => {
         }
     }
 
-    const values = { books, updateBook };
+    const values = { books, updateBook, createBooks };
     return (
         <BookContext.Provider value={values}>
             {props.children}
         </BookContext.Provider>
     )
-=======
-	const [books, setBooks] = useState([]);
-
-	useEffect(() => {
-		getBooks();
-	}, []);
-
-	const getBooks = async () => {
-		try {
-			const result = await get_books_service();
-			if (result.status === 200) {
-				setBooks(result.data);
-			}
-		} catch (error) {
-			console.log("error--in get method--", error);
-			return error
-		}
-	}
-
-	const updateBook = async (data) => {
-		try {
-			const result = await update_book_service(data);
-			if (result.status === 200) {
-				await getBooks()
-			}
-		} catch (error) {
-			console.log("error--in update--", error);
-			return error
-		}
-	}
-
-	const values = { books, updateBook };
-	return (
-		<BookContext.Provider value={values}>
-			{props.children}
-		</BookContext.Provider>
-	)
->>>>>>> upstream/main
 }
 
 const useBookContext = () => { return useContext(BookContext) }
